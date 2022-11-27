c6fdcc9cc59f771a072c4f7666e14ac0c35693a6
phcPedro <pedroohenrique.pedro@gmail.com>
2022-11-27 11:40:00 -0300

    feat: Implement Swagger in dto, app, houe

--- src/house/house.controller.ts
+++ src/house/house.controller.ts
@@ -1,7 +1,10 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { CreateHouseDto } from "./dto/create-house.dto";
 import { HouseService } from "./house.service";
 
+@ApiTags(
+)
 @Controller('house')
 export class HouseController{
  constructor(private houseService: HouseService){}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+   .setTitle('Dunas-House')
+   .setDescription('Application to help realtors in the presentation and sale of houses')
+   .setVersion('0.0.1 ALPHA')
+   .addTag('status')
+   .addTag('Home')
+   .build();
+
+   const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
