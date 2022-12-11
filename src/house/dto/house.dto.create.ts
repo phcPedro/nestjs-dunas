608a4b1578933f24d3feb9e83ffae251cbf76455
phcPedro <pedroohenrique.pedro@gmail.com>
2022-12-11 02:47:47 -0300

    feat: add more fields in HomeDB

--- src/house/dto/house.dto.create.ts
+++ src/house/dto/house.dto.create.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString, IsUrl, IsUUID } from "class-validator";
+import { isLocale, IsString, IsUrl} from "class-validator";
 
 export class CreateHouseDto{
 
@@ -19,5 +19,25 @@ export class CreateHouseDto{
   })
   img: string;
 
- 
+  @IsString()
+  @ApiProperty({
+    description:"Valor monetario do imovel.",
+    example:"1.150.000"
+  })
+  value: string;
+
+  @IsString()
+  @ApiProperty({
+    description:"Endereço do imovel.",
+    example:"Rua Dário Manoel Cardoso, 2860 - Ingleses do Rio Vermelho, Florianópolis - SC"
+  })
+  location: string;
+
+  @IsString()
+  @ApiProperty({
+    description:"Diferente do titulo, coloque mais informçãoes sobre o imovel.",
+    example:"O sobrado no bairro Ingleses do Rio Vermelho com 150 metros quadrados com 3 quartos sendo 2 suítes e 3 banheiros  Vai lhe possibilitar curtir os dias mais quentes na piscina. Já está com os móveis. Possui circuito de segurança, piscina e jacuzzi, todo em mármore branco."
+  })
+  information: string
+
 }
--- src/house/entities/house.entitys.ts
+++ src/house/entities/house.entitys.ts
@@ -1,6 +1,9 @@
 export class House{
   id?: string;
   title: string;
+  value: string;
+  location:  string;
+  information: string;
   img: string;
   createdAt?: Date;
   updatedAt?:Date;
